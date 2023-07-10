1dd13a07219628c1e94de4e16e14f83bace4cb4f
atilio ferreira <atilio.silvaferreira@gmail.com>
2023-07-10 20:12:04 -0300

    criando componente de form

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,89 +1,13 @@
-import {
-  Bank,
-  CreditCard,
-  CurrencyDollar,
-  MapPinLine,
-  Money,
-  Trash,
-} from 'phosphor-react'
+import { Trash } from 'phosphor-react'
 import coffeImage from '../../assets/coffees/expresso-americano.png'
 import { OrderCounterForm } from '../../components/OrderCounterForm'
-import {
-  CardContainer,
-  GridContainer,
-  PaymentOption,
-  PaymentOptionContainer,
-  CardTitle,
-  FormGrid,
-  FormGridItem,
-} from './styles'
+import { CardContainer, GridContainer } from './styles'
+import { FormCheckout } from './components/FormCheckout'
 
 export function Checkout() {
   return (
     <GridContainer>
-      <div>
-        <h2>Complete seu pedido</h2>
-        <CardContainer>
-          <CardTitle colorIcon="yellow-dark">
-            <MapPinLine size={22} />
-            <div>
-              <h3>Endereço de Entrega</h3>
-              <h4>Informe o endereço onde deseja receber seu pedido</h4>
-            </div>
-          </CardTitle>
-
-          <FormGrid>
-            <FormGridItem startColumn={1} columnSize={4}>
-              <input type="text" placeholder="CEP" />
-            </FormGridItem>
-            <FormGridItem startColumn={1} columnSize={12}>
-              <input type="text" placeholder="Rua" />
-            </FormGridItem>
-            <FormGridItem startColumn={1} columnSize={4}>
-              <input type="number" placeholder="Número" />
-            </FormGridItem>
-            <FormGridItem startColumn={5} columnSize={8}>
-              <input type="text" placeholder="Complemento" />
-            </FormGridItem>
-            <FormGridItem startColumn={1} columnSize={4}>
-              <input type="text" placeholder="Bairro" />
-            </FormGridItem>
-            <FormGridItem startColumn={5} columnSize={6}>
-              <input type="text" placeholder="Cidade" />
-            </FormGridItem>
-            <FormGridItem startColumn={11} columnSize={2}>
-              <input type="text" placeholder="UF" />
-            </FormGridItem>
-          </FormGrid>
-        </CardContainer>
-
-        <CardContainer>
-          <CardTitle colorIcon="purple">
-            <CurrencyDollar size={22} />
-            <div>
-              <h3>Pagamento</h3>
-              <h4>
-                O pagamento é feito na entrega. Escolha a forma que deseja pagar
-              </h4>
-            </div>
-          </CardTitle>
-
-          <PaymentOptionContainer>
-            <PaymentOption>
-              <CreditCard size={16} />
-              CARTÃO DE CRÉDITO
-            </PaymentOption>
-            <PaymentOption selected={true}>
-              <Bank size={16} />
-              CARTÃO DE DÉBITO
-            </PaymentOption>
-            <PaymentOption>
-              <Money size={16} />
-              DINHEIRO
-            </PaymentOption>
-          </PaymentOptionContainer>
-        </CardContainer>
-      </div>
+      <FormCheckout />
 
       <div>
         <h2>Cafés selecionados</h2>
